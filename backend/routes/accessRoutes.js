7306d918d1e8543e01b6d485facea64e29bacec3
Preethi <24z256@psgtech.ac.in>
2025-11-13 09:43:32 +0000

    Portal access

---
+++ backend/routes/accessRoutes.js
@@ -0,0 +1,11 @@
+// routes/portalRoutes.js
+import express from 'express';
+import { authenticateToken, authorizeYear } from '../middleware/authMiddleware.js';
+import { getYear1Portal, getYear2Portal } from '../controllers/portalController.js';
+
+const router = express.Router();
+
+router.get('/year1', authenticateToken, authorizeYear(1), getYear1Portal);
+router.get('/year2', authenticateToken, authorizeYear(2), getYear2Portal);
+
+export default router;
